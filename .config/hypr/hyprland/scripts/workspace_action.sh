3e20fdb2df0fdea70bff3f5926ed48f7a6ee81de
ashmartin1810 <ashmartin1810@gmail.com>
2025-08-19 20:21:39 +0530

    Feat: Added relative workspace support.

--- .config/hypr/hyprland/scripts/workspace_action.sh
+++ .config/hypr/hyprland/scripts/workspace_action.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
-curr_workspace="$(hyprctl monitors -j | jq -r ".[].activeWorkspace.id")" ##parses json output of hyprctl monitors
-dispatcher="$1"; shift ##Any dispatcher that hyprland supports, the shift the arguments such that $2 is not $1
-if [[ "$1" == *"+"* || "$1" == *"-"* ]]; then ##pattern matching
+curr_workspace="$(hyprctl activeworkspace -j | jq -r ".id")" ##parses json output of hyprctl activeworkspace on the active monitor
+dispatcher="$1"
+shift ##Any dispatcher that hyprland supports, the shift shifts the target workspace number such that target is now in $1, not $2
+
+if [[ -z "${dispatcher}" || "${dispatcher}" == "--help" || "${dispatcher}" == "-h" || -z "$1" ]]; then
+  echo "Usage: $0 <dispatcher> <target workspace>"
+  exit 1
+fi
+if [[ "$1" == *"+"* || "$1" == *"-"* ]]; then ##pattern matching (works with r+1 and +1 only aswell)
   hyprctl dispatch "${dispatcher}" "$1" ##$1 = workspace id since we shifted earlier.
-elif [[ "$1" =~ ^[0-9] ]]; then ##Regex matching
-  target_workspace=$(((($curr_workspace - 1) / 10 ) * 10 + $1)) ##decreases curr_workspace by 1, then floor division by 10. then multiplica
-                                                                ##ion by 10, and then adding $1. for eg, if $curr_workspace=24, and $1 = 6,
-                                                                ##((24-1)/10)*10 + 6
-                                                                ##(23 / 10) * 10 + 6
-                                                                ##2*10+6
-                                                                ##26
+elif [[ "$1" =~ ^[0-9]+$ ]]; then ##Regex matching
+  target_workspace=$(((($curr_workspace - 1) / 10 ) * 10 + $1))
   hyprctl dispatch "${dispatcher}" "${target_workspace}"
+else
+ hyprctl dispatch "${dispatcher}" "$1" ##Incase the dispatcher is something like special workspace which takes a string target. 
+ exit 1
 fi
